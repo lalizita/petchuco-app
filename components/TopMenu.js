e87fc563a284e981b652ff22ffd81302285b4fdf
Laís Lima <lais@cosmobots.io>
2019-10-10 20:28:32 -0300

    criando a homescreen

---
+++ components/TopMenu.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class TopMenu extends Component{
+  render(){
+    return(
+      <h1>hello</h1>
+    )
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -27,21 +27,31 @@ import {
   createStackNavigator,
   createAppContainer
 } from 'react-navigation';
-import Products from '../pages/Products';
-import Checkout from '../pages/Checkout';
-import Receipt from '../pages/Receipt';
+import Products from '../screens/Products';
+import Checkout from '../screens/Checkout';
+import Receipt from '../screens/Receipt';
+import HomeScreen from '../screens/HomeScreen';
 import themes from '../styles/theme.style';
 
 const RootStack = createStackNavigator({
-Products: {
-  screen: Products
-},
-Checkout: {
-  screen: Checkout
-},
-Receipt: {
-  screen: Receipt
-}
+Products,
+Checkout,
+Receipt,
+Home: HomeScreen
+}, 
+{
+  initialRouteParams: 'Home',
+  initialRouteName: 'Home',
+  /* The header config from HomeScreen is now here */
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: '#31b6eb',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  }
 });
 
 const App = createAppContainer(RootStack);
